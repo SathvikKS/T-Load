454391b2b9fe7a842980997eb196094a0fab870d
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-10-30 21:11:01 +0530

    Update and rename T-Embed/setup.sh to setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,7 +1,7 @@
 echo "Need to install dependencies. Makesure to have Netconnection. Please type y during installation process"
+cd T-Embed
 chmod +x update.sh
 chmod +x tembed.sh
-chmod +x easy.sh
 chmod +x upload.sh
 chmod +x ssh.sh
 chmod +x rmhost.sh
@@ -9,7 +9,7 @@ chmod +x master.sh
 chmod +x download.sh
 chmod +x core.sh
 chmod +x logout.sh
-chmod +x redownload.sh
 chmod +x clear.sh
 apt-get install expect
 pkg install openssh
+cd ..
