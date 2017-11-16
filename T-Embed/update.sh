cffae5bfb16249892e6eb83cdf423d8920765a8e
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-11-16 21:39:23 +0530

    Update update.sh

--- T-Embed/update.sh
+++ T-Embed/update.sh
@@ -1,14 +1,15 @@
 clear
 echo "Updating files"
-curl -LO https://mucronate-jay-0792.dataplicity.io/download.sh               curl -LO https://mucronate-jay-0792.dataplicity.io/upload.sh
-curl -LO https://mucronate-jay-0792.dataplicity.io/ssh.sh
-curl -LO https://mucronate-jay-0792.dataplicity.io/T-Load/T-Embed/tembed.sh
-curl -LO https://mucronate-jay-0792.dataplicity.io/T-Load/T-Embed/master.sh
-curl -LO https://mucronate-jay-0792.dataplicity.io/T-Load/T-Embed/logout.sh
-curl -LO https://mucronate-jay-0792.dataplicity.io/T-Load/T-Embed/core.sh
-curl -LO https://mucronate-jay-0792.dataplicity.io/T-Load/T-Embed/rmhost.sh
-curl -LO https://mucronate-jay-0792.dataplicity.io/T-Load/T-Embed/clear.sh
-curl -LO https://mucronate-jay-0792.dataplicity.io/T-Load/T-Embed/update.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/corefiles/download.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/corefiles/upload.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/corefiles/ssh.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/tembed.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/master.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/logout.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/core.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/rmhost.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/clear.sh
+curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/update.sh
 cd ..
 ./setup2.sh
 cd T-Embed
