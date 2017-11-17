62b13c06e86d941cf1fa7761bba106a2a3e807d4
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-11-17 05:31:17 +0530

    Update update.sh

--- T-Embed/update.sh
+++ T-Embed/update.sh
@@ -3,13 +3,12 @@ echo "Updating files"
 curl -LO https://toothier-wombat-0319.dataplicity.io/corefiles/download.sh
 curl -LO https://toothier-wombat-0319.dataplicity.io/corefiles/upload.sh
 curl -LO https://toothier-wombat-0319.dataplicity.io/corefiles/ssh.sh
-curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/tembed.sh
-curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/master.sh
-curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/logout.sh
-curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/core.sh
-curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/rmhost.sh
-curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/clear.sh
-curl -LO https://toothier-wombat-0319.dataplicity.io/T-Load/T-Embed/update.sh
+curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/tembed.sh
+curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/master.sh
+curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/logout.sh
+curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/core.sh
+curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/rmhost.sh
+curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/clear.sh
 cd ..
 ./setup2.sh
 cd T-Embed
