571fbdcc8fe33fe39a8a9f72ace116187a1f13f3
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-11-20 05:22:22 +0530

    Update upload.sh

--- T-Embed/upload.sh
+++ T-Embed/upload.sh
@@ -1,8 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/expect
+source /data/data/com.termux/files/home/T-Load/T-Embed/port
 puts -nonewline "Please enter the ORIGINAL apk path WITH .apk extension: "
 flush stdout
 gets stdin p1
-spawn scp -P 10133 $p1 sathvik@0.tcp.ngrok.io:/home/sathvik/payload
+spawn scp -P $ppoort $p1 sathvik@0.tcp.ngrok.io:/home/sathvik/payload
 expect "Are you sure you want to continue connecting (yes/no)? "
 send "yes\r"
 expect "sathvik@0.tcp.ngrok.io's password: "
