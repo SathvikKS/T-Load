a0de68a40beb05e2806584c1f29fb895e91be0fb
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-11-19 17:20:33 +0530

    Update server.sh

--- Server/version-2/server.sh
+++ Server/version-2/server.sh
@@ -1,3 +1,6 @@
-echo 'This is no longer supported. Downloading new one"
+clear
+echo "version = 2.0Beta"
+sleep
+echo 'OOPS!!! This is no longer supported. Downloading new one"
 cd $HOME/T-Load
 ./redownload.sh
