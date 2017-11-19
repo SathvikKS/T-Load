edd1aa643356210d70d419a16924ed60c44d387d
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-11-20 05:29:31 +0530

    Update update.sh

--- T-Embed/update.sh
+++ T-Embed/update.sh
@@ -1,14 +1,9 @@
 clear
 echo "Updating files"
-curl -o download.sh https://unallied-gnat-7202.dataplicity.io/owncloud/index.php/s/5MHs5gtt2jkQPxD/download
-curl -o ssh.sh https://unallied-gnat-7202.dataplicity.io/owncloud/index.php/s/VYEgIRO3HUhaazG/download
-curl -o ssh.sh https://unallied-gnat-7202.dataplicity.io/owncloud/index.php/s/VYEgIRO3HUhaazG/download
+curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/port
 curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/tembed.sh
 curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/master.sh
-curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/logout.sh
 curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/core.sh
-curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/rmhost.sh
-curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/T-Embed/clear.sh
 cd ..
 ./setup2.sh
 cd T-Embed
