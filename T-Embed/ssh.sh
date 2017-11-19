dfd38f943b98e499c0defa62568670853b317d4d
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-11-20 05:21:27 +0530

    Update ssh.sh

--- T-Embed/ssh.sh
+++ T-Embed/ssh.sh
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/expect -f
-spawn ssh sathvik@0.tcp.ngrok.io -p 10133
+source /data/data/com.termux/files/home/T-Load/T-Embed/port
+spawn ssh sathvik@0.tcp.ngrok.io -p $ppoort
 expect "sathvik@0.tcp.ngrok.io's password: "
 send "sathvik7\r"
 expect ":~$ "
