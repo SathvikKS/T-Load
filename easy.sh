619f48819a73f8531a89a086b186569f9cb6ecf2
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-11-20 05:34:18 +0530

    Update easy.sh

--- easy.sh
+++ easy.sh
@@ -1,5 +1,5 @@
 clear
-curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/Server/version3.0/server.sh
+curl -LO https://raw.githubusercontent.com/SathvikKS/T-Load/master/Server/version3/server.sh
 clear
 ./server.sh
 echo "----------------------------------"
