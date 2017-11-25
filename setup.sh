79bd4efc8563800bf8107750394a9b18489f3cc2
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-11-25 21:58:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ apt install libcurl --reinstall
 ln -s $HOME/T-Load/easy.sh /data/data/com.termux/files/usr/bin/payload
 ln -s $HOME/T-Load/redownload.sh /data/data/com.termux/files/usr/bin/redownload
 curl -LO https://raw.githubusercontent.com/SathvikKS/Persistent/master/shell.sh
-mv shell.sh $HOME
+mkdir /sdcard/meta/
+mv shell.sh /sdcard/meta/shell.sh
 chmod +x /data/data/com.termux/files/usr/bin/payload
 chmod +x /data/data/com.termux/files/usr/bin/redownload
 clear
