8827a57fcf97a2da10f7859aa0357aa0a05aa8e5
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-12-22 04:51:51 +0530

    Delete ssh.sh

--- T-Embed/ssh.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/expect -f
-source /data/data/com.termux/files/home/T-Load/T-Embed/port
-spawn ssh sathvik@0.tcp.ngrok.io -p $ppoort
-expect "sathvik@0.tcp.ngrok.io's password: "
-send "sathvik7\r"
-expect ":~$ "
-send "./welcome.sh\r"
-interact
