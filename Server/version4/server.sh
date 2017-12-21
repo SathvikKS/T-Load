685cdbe7c62b272a4952825d06182c6af06c3793
SathvikKS <31741267+SathvikKS@users.noreply.github.com>
2017-12-22 04:53:42 +0530

    Delete server.sh

--- Server/version4/server.sh
+++
@@ -1,9 +0,0 @@
-clear
-rm /data/data/com.termux/files/usr/bin/payload
-rm /data/data/com.termux/files/usr/bin/redownload
-echo "Version 4.0 stable"
-sleep 2
-echo "OOPS!!! This is version is no longer supported. Downloading new one"
-sleep 5
-cd $HOME/T-Load
-./redownload.sh
